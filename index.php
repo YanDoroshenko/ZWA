e91588dc28cefc22176b573a9daf41f0d67157f0
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-07-01 10:56:52 +0200

    [new] DB connection and querying

--- index.php
+++ index.php
@@ -8,9 +8,12 @@
 Before
 <br/>
 <?php
-echo "Hello world"
+$db = mysqli_connect("localhost", "root", "L0v3_7h3,5m311.0f_n@p@1m,in.7h3_m0rning", "mysql");
+$query = mysqli_query($db, "SELECT id, name, country FROM test_users");
+foreach (mysqli_fetch_all($query) as $result)
+    echo $result[0] . " " . $result[1] . " " . $result[2] . "<br/>";
+mysqli_close($db);
 ?>
-<br/>
 After
 </body>
 </html>

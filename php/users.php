b7d307072608474dab2e744b07e4bd6713918157
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-07-03 13:21:55 +0200

    [new] Users page

---
+++ php/users.php
@@ -0,0 +1,30 @@
+<?php
+ob_start();
+include("header.php");
+session_start();
+require_once 'db.php';
+
+// if session is not set this will redirect to login page
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+    exit;
+}
+// select loggedin users detail
+$users = $db->query("SELECT * FROM t_user");
+?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>Users</title>
+    </head>
+    <body>
+
+    <?php
+    while ($user = $users->fetch_assoc())
+        echo $user['id'] . " " . $user['login'] . " " . $user['name'] . "<br/>";
+    ?>
+
+    </body>
+    </html>
+<?php ob_end_flush(); ?>

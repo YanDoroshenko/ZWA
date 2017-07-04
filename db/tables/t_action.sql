3862e54a17e7fe08a7c50a8796b2e6feaf5dad45
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-07-04 09:35:38 +0200

    [upd] Changed DB structure

--- db/tables/t_action.sql
+++ db/tables/t_action.sql
@@ -1,11 +1,15 @@
 CREATE TABLE t_action (
-  id            INT AUTO_INCREMENT,
-  name          VARCHAR(55) NOT NULL UNIQUE,
-  description   VARCHAR(255),
-  source_status INT         NOT NULL,
-  target_status INT         NOT NULL,
+  id            INT                AUTO_INCREMENT,
+  task          INT,
+  source_status INT,
+  target_status INT,
+  actor         INT,
+  timepoint     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  description   VARCHAR(1000),
   PRIMARY KEY (id),
+  FOREIGN KEY (task) REFERENCES t_task (id),
   FOREIGN KEY (source_status) REFERENCES t_status (id),
   FOREIGN KEY (target_status) REFERENCES t_status (id),
-  UNIQUE (source_status, target_status)
+  FOREIGN KEY (actor) REFERENCES t_user (id),
+  CHECK (source_status != t_action.target_status OR description IS NOT NULL)
 );

f13938ca53005536720940adf6a27d0f44656f6d
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-07-05 17:02:02 +0200

    [upd] Cleanup

--- db/data/t_action.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO t_action (name, description, source_status, target_status)
-VALUES ('Process', 'Task is being processed', 1, 2);
-INSERT INTO t_action (name, description, source_status, target_status)
-VALUES ('Complete', 'Task is completed', 2, 3);

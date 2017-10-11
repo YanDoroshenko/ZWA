aec16b95a67dedb85eeb93698c9555ad12d7a7c0
Yan Doroshenko <yan1994doroshenko@gmail.com>
2017-10-11 13:14:18 +0200

    [upd] DB connection credentials

--- php/db.php
+++ php/db.php
@@ -1,12 +1,13 @@
 <?php
 
 define('DBHOST', 'localhost');
-define('DBUSER', 'root');
-define('DBPASS', 'L0v3_7h3,5m311.0f_n@p@1m,in.7h3_m0rning');
-define('DBNAME', 'mysql');
+define('DBUSER', 'dorosyan');
+define('DBPASS', 'webove aplikace');
+define('DBNAME', 'dorosyan');
 
 $db = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
 
-if (!$db) {
+if (!$db)
     die("Database connection failed : " . $db->error);
-}
+else
+    $db->set_charset("utf8");

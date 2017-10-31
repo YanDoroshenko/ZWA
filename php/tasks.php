fb65f55fdc52b939486a2b44929368ac405e8073
Yan Doroshenko <dorosyan@fel.cvut.cz>
2017-10-31 09:46:37 +0000

    [new] Tasks filtering

--- php/tasks.php
+++ php/tasks.php
@@ -8,9 +8,15 @@ if (!isset($_SESSION['user'])) {
     exit;
 }
 
+if (isset($_POST['btn-filter']))
+    $filter = '%' . $_POST['filter'] . '%';
+else
+    $filter = '%';
+
 $page_size = 5;
 
-$count_query = $db->prepare("SELECT count(*) FROM t_task");
+$count_query = $db->prepare("SELECT count(*) FROM t_task WHERE name LIKE ? OR description LIKE ?");
+$count_query->bind_param("ss", $filter, $filter);
 if (!$count_query || !$count_query->execute()) {
     echo $query->error;
     echo $db->error;
@@ -32,7 +38,8 @@ else {
 $from = min($count, $offset + 1);
 $to = min($count, $offset + $page_size);
 
-$query = $db->prepare("SELECT t.id 'id', t.name 'name', priority, s.title 'status', r.login 'reporter', a.login 'assignee' FROM t_task t JOIN t_status s ON t.status = s.id JOIN t_user r ON t.reporter = r.id LEFT JOIN t_user a ON t.assignee = a.id LIMIT $offset, $page_size");
+$query = $db->prepare("SELECT t.id 'id', t.name 'name', priority, s.title 'status', r.login 'reporter', a.login 'assignee' FROM t_task t JOIN t_status s ON t.status = s.id JOIN t_user r ON t.reporter = r.id LEFT JOIN t_user a ON t.assignee = a.id WHERE t.name LIKE ? OR t.description LIKE ? LIMIT $offset, $page_size");
+$query->bind_param("ss", $filter, $filter);
 if (!$query || !$query->execute()) {
     echo $query->error;
     echo $db->error;
@@ -55,18 +62,26 @@ else {
 include("header.php");
 ?>
     <a href="new_task.php">New task</a>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	<input type="text" name="filter" placeholder="Filter"
+	value="<?php
+if (isset($filter))
+    echo str_replace("%", "", $filter); ?>"
+	/>
+	<button type="submit" name="btn-filter">Filter</button>
+    </form>
 <br/>
 <?php
-if (isset($tasks))
-    while ($task = $tasks->fetch_assoc()) {
-	echo "<a href=task_detail.php?id=" . $task['id'] . ">" . $task['id'] . "</a> ";
-	echo $task['name'] . " ";
-	echo $task['priority'] . " ";
-	echo $task['status'] . " ";
-	echo $task['reporter'] . " ";
-	echo $task['assignee'] . " ";
-	echo "<br/>";
-    }
+    if (isset($tasks))
+	while ($task = $tasks->fetch_assoc()) {
+	    echo "<a href=task_detail.php?id=" . $task['id'] . ">" . $task['id'] . "</a> ";
+	    echo $task['name'] . " ";
+	    echo $task['priority'] . " ";
+	    echo $task['status'] . " ";
+	    echo $task['reporter'] . " ";
+	    echo $task['assignee'] . " ";
+	    echo "<br/>";
+	}
 if ($from > 1)
     echo "<a href=\"tasks.php?page=" . intval($page - 1) . "\">Previous page<a/>";
 if ($to < $count)

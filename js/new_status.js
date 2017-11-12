4b2d6ebabb73797e9f50f69ca60362a4de87bfee
Yan Doroshenko <dorosyan@fel.cvut.cz>
2017-11-12 13:19:35 +0000

    [upd] Improve validation

--- js/new_status.js
+++ js/new_status.js
@@ -20,15 +20,53 @@ title.addEventListener("blur", (e) => {
     }
 });
 
+const iconUpload = document.getElementById("iconUpload");
+const iconUploadFeedback = document.getElementById("iconUpload-feedback");
+iconUpload.addEventListener("change", (e) => {
+    if (iconUpload.value) {
+        if (/(\.jpg|\.jpeg|\.bmp|\.gif|\.png)$/i.exec(iconUpload.value)) {
+            iconUpload.classList.remove("incorrect");
+            iconUpload.classList.add("correct");
+            iconUploadFeedback.classList.remove("incorrect");
+            iconUploadFeedback.classList.add("correct");
+            iconUploadFeedback.innerHTML = "\u2714";
+        }
+        else {
+            iconUpload.classList.remove("correct");
+            iconUpload.classList.add("incorrect");
+            iconUploadFeedback.classList.remove("correct");
+            iconUploadFeedback.classList.add("incorrect");
+            iconUploadFeedback.innerHTML = "\u2716 File is not supported";
+        }
+    }
+});
+
+
 // Entire form validation before submit
 const form = document.getElementById("new_status");
 form.addEventListener("submit", (e) => {
-    if (title.value) {
+    if (!title.value) {
         e.preventDefault();
         title.classList.add("incorrect");
         title.classList.remove("correct");
         titleFeedback.classList.add("incorrect");
         titleFeedback.classList.remove("correct");
         titleFeedback.innerHTML = "\u2716 Title should not be empty";
+    }
+    if (iconUpload.value) {
+        if (/(\.jpg|\.jpeg|\.bmp|\.gif|\.png)$/i.exec(iconUpload.value)) {
+            iconUpload.classList.remove("incorrect");
+            iconUpload.classList.add("correct");
+            iconUploadFeedback.classList.remove("incorrect");
+            iconUploadFeedback.classList.add("correct");
+            iconUploadFeedback.innerHTML = "\u2714";
+        }
+        else {
+            iconUpload.classList.remove("correct");
+            iconUpload.classList.add("incorrect");
+            iconUploadFeedback.classList.remove("correct");
+            iconUploadFeedback.classList.add("incorrect");
+            iconUploadFeedback.innerHTML = "\u2716 File is not supported";
+        }
     }
 });
